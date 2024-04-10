321ef840f906a6669e95faa3ad82f2f9c9ecb92c
mitra-tech <davoudabadi.mitra@gmail.com>
2024-04-10 17:08:32 -0600

    adds the Auction properties to the Entities.

--- src/AuctionService/obj/Release/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Release/net8.0/AuctionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280f891c52076091bf363d41d15b1582e108c42e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
