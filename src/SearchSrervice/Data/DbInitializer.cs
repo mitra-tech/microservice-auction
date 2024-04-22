e51f92e54045413cbe1e6a1571b1ef92cfc38bab
mitra-tech <davoudabadi.mitra@gmail.com>
2024-04-22 14:48:15 -0600

    adds the Http communication between to SearchServices and AuctionService

--- src/SearchSrervice/Data/DbInitializer.cs
+++ src/SearchSrervice/Data/DbInitializer.cs
@@ -18,16 +18,15 @@ public class DbInitializer
             .CreateAsync();
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0) 
-        {
-        Console.WriteLine("No data - is seeded!");
-        var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+       using var scope = app.Services.CreateScope();
 
-        var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+       var httpClient = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
+       
+       var items = await httpClient.GetItemsForSearchDb();
 
-        var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+       Console.WriteLine(items.Count + "returned from the auction service");
 
-        await DB.SaveAsync(items);
-        } 
+       if (items.Count > 0) await DB.SaveAsync(items);
+       
     }
 }
--- src/SearchSrervice/obj/Debug/net8.0/SearchSrervice.AssemblyInfo.cs
+++ src/SearchSrervice/obj/Debug/net8.0/SearchSrervice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchSrervice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c72dfafc2a898f3d000e794175981c853e8229f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c7e31f53f1ab43f4e98801c978a19b1590973a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchSrervice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchSrervice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
