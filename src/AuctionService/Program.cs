832f0d28a33871117f21355cf6a4d5a50aead425
mitra-tech <davoudabadi.mitra@gmail.com>
2024-04-25 21:41:48 -0600

    adds the Inbox and outbox messages tables

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -17,6 +17,15 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // With the following set up, it automatically configures the names of our ques
 builder.Services.AddMassTransit(x => 
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext> (o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UsePostgres();
+
+        o.UseBusOutbox();
+    });
+
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2adf22cf9af00beb75ff7831668be1a22adce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bd9c92e261cb33b323f75c66c504161b8c994c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
