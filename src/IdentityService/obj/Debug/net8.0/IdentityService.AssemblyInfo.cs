f77ca11ed192825165d270a06bb772bb0af34a5f
mitra-tech <davoudabadi.mitra@gmail.com>
2024-04-28 11:07:57 -0600

    Adds the register page

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d95d1b549c5331537d50789819e0bb6cc585970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18065d70cb6b19e1c8214cf520d74d59aa67f1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
