f18c1817d9fb0ce01d5e05f069100dff3e81608a
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-06 15:35:34 -0600

    Adds the configuration to the .yml file to run the IdentityService as a container

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25719697ca1bc44f56df1ac425fd24e7438b2fff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c092f9caefc294438770d9272140bd945780ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
