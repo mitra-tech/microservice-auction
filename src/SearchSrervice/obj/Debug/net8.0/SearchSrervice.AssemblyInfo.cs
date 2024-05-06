ec80069431f432ab7a29b9137016b55d3744d558
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-06 16:21:38 -0600

    Adds the configuration to the .yml file to run the GatewayService as a container

--- src/SearchSrervice/obj/Debug/net8.0/SearchSrervice.AssemblyInfo.cs
+++ src/SearchSrervice/obj/Debug/net8.0/SearchSrervice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchSrervice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6026417efbc6c85db6319e29aaa4f4adeb8c98af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18c1817d9fb0ce01d5e05f069100dff3e81608a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchSrervice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchSrervice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
