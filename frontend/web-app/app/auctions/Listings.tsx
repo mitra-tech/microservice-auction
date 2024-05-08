5de21ac45f2428d3f2747a36a5fbde4d60c5ab7e
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-08 15:04:13 -0600

    Adds pagination to the web app

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import AuctionCard from './AuctionCard';
 import { Auction, PageResult } from '@/types/Index';
+import AppPagination from '../components/AppPagination';
 
 
 // Fetchs data from serch service
 async function getData() : Promise<PageResult<Auction>>{
-    const res = await fetch('http://localhost:6001/search');
+    const res = await fetch('http://localhost:6001/search?pageSize=4');
+
     if(!res.ok) throw new Error('Failed to ferch data!');
+    
     return res.json();
   }
 
@@ -15,10 +18,15 @@ export default async function Listings() {
     const data = await getData();
 
   return (
+    <>
     <div className='grid grid-cols-4 gap-6'>
         {data && data.results.map((auction) => (
             <AuctionCard auction={auction} key={auction.id}/>
         ))}
     </div>
+    <div className='flex justify-center mt-4'>
+      <AppPagination currentPage={1} pageCount={data.pageCount}/>
+    </div>
+    </>
   )
 }
--- frontend/web-app/tailwind.config.ts
+++ frontend/web-app/tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
 
+/** @type {import('tailwindcss').Config} */
+
+const flowbite = require("flowbite-react/tailwind");
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -19,7 +24,8 @@ const config: Config = {
     aspectRatio: false
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    flowbite.plugin(),
   ],
 };
 export default config;
