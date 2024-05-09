d42cac562226e4ebf0ab287f8b4f5c3a0c727d52
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-09 12:05:46 -0600

    Uses Zustand for state management

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,3 +1,4 @@
+import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'; 
 
 type Props = {
@@ -10,7 +11,17 @@ export default function Filters({pageSize, setPageSize}: Props) {
   return (
     <div className='flex justify-between items-center mb-4'>
         <div>
-            <span className='uppercase text-sm text-gray-400 mr-2'> </span>
+            <span className='uppercase text-sm text-gray-400 mr-2'>Page size</span>
+            <ButtonGroup>
+                {pageSizeButtons.map((value, index) => (
+                    <Button key={index} 
+                    onClick={() => setPageSize(value)}
+                    color={`${pageSize === value ? 'red' : 'gray'}`}
+                    >
+                        {value}
+                    </Button>
+                ))}
+            </ButtonGroup>
         </div>
     </div>
   )
