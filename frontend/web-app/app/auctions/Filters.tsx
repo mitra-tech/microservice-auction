1456912a12491788602aa3d674c7d27bde8edf33
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-11 10:54:50 -0600

    Adds the orderBy functionality

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,17 +1,53 @@
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'; 
+import { AiOutlineClockCircle, AiOutlineSortAscending } from 'react-icons/ai';
+import { BsFillStopCircleFill } from 'react-icons/bs';
 
 
 const pageSizeButtons = [4, 8, 12];
 
+const orderButtons = [
+    {
+    label: "Alphabetical",
+    icon: AiOutlineSortAscending,
+    value: 'make',
+    },
+    {
+        label: "End date",
+        icon: AiOutlineClockCircle,
+        value: 'endingSoon',
+    },
+
+    {
+        label: "Recently added",
+        icon: BsFillStopCircleFill,
+        value: 'new',
+    },
+];
+
 export default function Filters() {
 
   const pageSize = useParamsStore(state => state.pageSize);  
-  const setParams = useParamsStore(state => state.setParams)
+  const setParams = useParamsStore(state => state.setParams);
+  const orderBy = useParamsStore(state => state.orderBy);
 
   return (
     <div className='flex justify-between items-center mb-4'>
+
+        <div>
+        <span className='uppercase text-sm text-gray-400 mr-2'>Order by</span>
+        <ButtonGroup>
+            {orderButtons.map(({label, icon: IconBase, value}) => (
+                <Button key={value} onClick={() => setParams({orderBy: value})} color={`${orderBy === value ? 'red' : 'gray'}`}>
+                    <IconBase className='mr-3 h-4 w-4' />
+                    {label}
+                </Button>
+            ))}
+        </ButtonGroup>
+        </div>
+
+
         <div>
             <span className='uppercase text-sm text-gray-400 mr-2'>Page size</span>
             <ButtonGroup>
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -17,7 +17,8 @@ export default function Listings() {
   const params = useParamsStore(state => ({
     pageNumber: state.pageNumber,
     pageSize: state.pageSize,
-    searchTerm: state.searchTerm
+    searchTerm: state.searchTerm,
+    orderBy: state.orderBy
   }), shallow);
 
 
