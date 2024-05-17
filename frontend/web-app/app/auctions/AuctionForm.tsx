dcad1a8aa41e9bb7176b012b6ff4885f110309f3
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-17 11:55:43 -0600

    Implement react datepicker as a date input

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -3,6 +3,7 @@ import { Button, TextInput } from 'flowbite-react';
 import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input';
+import DateInput from '../components/DateInput';
 
 export default function AuctionForm() {
 
@@ -35,7 +36,14 @@ export default function AuctionForm() {
 
         <div className='grid grid-cols-2 gap-3'>
             <Input lable='Reserve Price (enter 0 if no reserve)' name='reservePrice' control={control} type='number' rules={{required: "Reserve Price is required!"}}/>
-            <Input lable='Auction end date/time' name='auctionEnd' control={control} type='date' rules={{required: "Aiction end date is required!"}}/>
+            <DateInput 
+                lable='Auction end date/time' 
+                name='auctionEnd' 
+                control={control} 
+                dateFormat='dd MMMM yyyy h:mm a' 
+                showTimeSelect
+                rules={{required: "Aiction end date is required!"}}
+            />
         </div>
 
         <div className='flex justify-between'>
