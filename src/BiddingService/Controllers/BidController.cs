9b4670e355e29a04cd35ecf71b7656e774823fb9
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-22 11:23:45 -0600

    Add Bid Controller Api

---
+++ src/BiddingService/Controllers/BidController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BidsController : ControllerBase {
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount ) 
+    {
+        var auction  = await DB.Find<Auction>().OneAsync(auctionId);
+
+        if (auction == null) 
+        {
+            // ToDo: check with auction service if it has auction
+            return NotFound();
+        };
+
+        if(auction.Seller == User.Identity.Name)
+        {
+            return BadRequest("You cant bid in your own auction");
+        };
+
+        var bid = new Bid 
+        {
+            Amount = amount,
+            AuctionId = auctionId,
+            Bidder = User.Identity.Name
+        };
+
+        if(auction.AuctionEnd < DateTime.UtcNow)
+        {
+            bid.BidStatus = BidStatus.Finished;
+        }
+
+        var highBid = await DB.Find<Bid>()
+            .Match(a => a.AuctionId == auctionId)
+            .Sort(b => b.Descending(x => x.Amount))
+            .ExecuteFirstAsync();
+
+        if (highBid != null && amount > highBid.Amount || highBid == null) 
+        {
+            bid.BidStatus = amount > auction.ReservePrice ? BidStatus.Accepted : BidStatus.AcceptedBelowReserve;
+        }
+
+        if (highBid != null &&  bid.Amount  <= highBid.Amount) 
+        {
+            bid.BidStatus = BidStatus.TooLow
+;       }
+
+        await DB.SaveAsync(bid);
+        return Ok(bid);
+    }
+}
--- src/BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
+++ src/BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2528f06be55986abcfc2e6d23705f7b146044ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8891157ee15937cee6b2eb963ade4c54067f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
