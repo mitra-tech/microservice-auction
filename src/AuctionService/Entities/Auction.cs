b8bc04921311d36493cdee2c840b68e4b54a445a
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-23 14:39:30 -0600

    Fix nullable Amount field

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -6,7 +6,7 @@ public class Auction
     public int ReservePrice { get; set; }
     public string Seller { get; set; }
     public string Winner { get; set; }
-    public int SoldAmount { get; set; }
+    public int? SoldAmount { get; set; }
     public int? CurrentHighBid { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
