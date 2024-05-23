8ba273d575d2a4dba08cc74f72c8a0724ee73db0
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-23 11:32:47 -0600

    Add a background service fot the auction finished event

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -45,6 +45,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHostedService<CheckAuctionFinished>();
 
 var app = builder.Build();
 
