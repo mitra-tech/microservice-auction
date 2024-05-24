cd6857dc66aa68261548f28d16b8d36373cab0a5
mitra-tech <davoudabadi.mitra@gmail.com>
2024-05-24 10:09:00 -0600

    Add gRPC to AuctionService

--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34493bf0f6d25067ec5e8be182486036c67c4d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bc04921311d36493cdee2c840b68e4b54a445a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
