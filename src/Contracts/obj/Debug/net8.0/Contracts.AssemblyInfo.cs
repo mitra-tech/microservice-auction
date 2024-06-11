44182d98839247de409bd6a96905a14d79c928a1
mitra-tech <davoudabadi.mitra@gmail.com>
2024-06-11 14:19:29 -0600

    Add unit test for the AuctionEntities

--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db49a0128a006a6e2543f621376757ffc972fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06ba285b8daf617ca2bf9593ea109e3b7e8e199")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
